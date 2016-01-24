233a1f84c035b5f6b501aa6154cf0dfd0cb229b5
zentoku <zentoku@sea.plala.or.jp>
2016-01-24 19:05:01 +0900

    バッチファイルを改良。

--- MasterCreate/Data/Csv/import.bat
+++ MasterCreate/Data/Csv/import.bat
@@ -1,5 +1,10 @@
 rem すごく。。。手抜きです。。。。
-rem ディレクトリの中身を抽出してループさせたかったんだけど力尽きたorz
-iconv -f Shift-JIS  -t UTF-8 test.csv > test.utf
-mysqlimport -u root --password=password --local --delete --fields-enclosed-by \" --ignore-lines=1  --fields-terminated-by , --lines-terminated-by \n --host 192.168.10.101 --default-character-set=utf8 test_master test.utf
+rem ディレクトリの中身を抽出してループさせたかったんだけど力尽きたorz 。。。って順番を自分で規定しないといけないんだった。ディレクトリ読む式は元からだめじゃん。
+rem というわけでファイルリストを自分で書いて読み込む式にした。
+for /f %%x in (importfiles.txt) do (
+    iconv -f Shift-JIS  -t UTF-8 %%x > %%x.utf 
+    mysqlimport -u root --password=password --local --delete --fields-enclosed-by \" --ignore-lines=1  --fields-terminated-by , --lines-terminated-by \n --host 192.168.10.101 --default-character-set=utf8 test_master %%x.utf
+    del %%x.utf
+)
+
 
