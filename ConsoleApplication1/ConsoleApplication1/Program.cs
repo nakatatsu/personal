f8c2f3bc30e6bc32ae951656c07141b27f9d3f20
zentoku <zentoku@sea.plala.or.jp>
2016-01-30 21:01:13 +0900

    いろいろ

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello world");
+        }
+    }
+}
